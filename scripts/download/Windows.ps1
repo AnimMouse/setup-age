9ff5c3f78894ba31d08d1a30f8244d0be6a23165
AnimMouse <git@animmouse.com>
2023-01-29 11:17:13 +0800

    fix: Add mkdir and use Invoke-WebRequest

--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading age $env:version for Windows
-Start-BitsTransfer $env:GITHUB_SERVER_URL/FiloSottile/age/releases/download/$env:version/age-$env:version-windows-amd64.zip
+Invoke-WebRequest $env:GITHUB_SERVER_URL/FiloSottile/age/releases/download/$env:version/age-$env:version-windows-amd64.zip -OutFile age-$env:version-windows-amd64.zip
 7z e age-$env:version-windows-amd64.zip age.exe age-keygen.exe -oage -r
 Remove-Item age-$env:version-windows-amd64.zip
 Write-Host ::endgroup::
