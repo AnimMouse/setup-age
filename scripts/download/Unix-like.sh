c18a617aab765869cdff669feeb5ad4b3fddf99b
Anim Mouse <git@animmouse.com>
2023-10-25 23:40:01 +0800

    fix: Use native commands for extracting on Windows and and use variables on specified version

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -3,5 +3,5 @@ set -eu
 echo ::group::Downloading age $version for $RUNNER_OS
 if [ $RUNNER_OS = macOS ]; then os=darwin; else os=linux; fi
 mkdir age
-wget -qO- $GITHUB_SERVER_URL/FiloSottile/age/releases/download/$version/age-$version-$os-amd64.tar.gz | tar -xz -C age --strip-components 1 age/age age/age-keygen
+wget -qO- $GITHUB_SERVER_URL/FiloSottile/age/releases/download/$version/age-$version-$os-amd64.tar.gz | tar -xzC age --strip-components 1 age/age age/age-keygen
 echo ::endgroup::
