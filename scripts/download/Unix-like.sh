d7fa8779bcb38294334a3aa753c7171e8da5067e
Anim Mouse <git@animmouse.com>
2025-12-28 11:12:10 +0800

    feat: Add support for arm64 macOS and Linux

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
-echo ::group::Downloading age $version for $RUNNER_OS
+echo ::group::Downloading age $version for $RUNNER_OS $RUNNER_ARCH
 if [ $RUNNER_OS = macOS ]; then os=darwin; else os=linux; fi
+if [ $RUNNER_ARCH = ARM64 ]; then arch=arm64; else arch=amd64; fi
 mkdir age
-wget -qO- $GITHUB_SERVER_URL/FiloSottile/age/releases/download/$version/age-$version-$os-amd64.tar.gz | tar -xzC age --strip-components 1 age/age age/age-keygen
+wget -qO- $GITHUB_SERVER_URL/FiloSottile/age/releases/download/$version/age-$version-$os-$arch.tar.gz | tar -xzC age --strip-components 1 age/age age/age-keygen
 echo ::endgroup::
